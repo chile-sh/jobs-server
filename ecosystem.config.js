cce5f076a50c74de8c44b4024ea12ef22f0a466c
Mauricio Allende <mauricio.allende.o@gmail.com>
2019-04-23 20:42:57 -0400

    add package.json and pm2 config

---
+++ ecosystem.config.js
@@ -0,0 +1,27 @@
+'use strict'
+
+const path = require('path')
+
+const isProd = process.env.NODE_ENV === 'production'
+const watch = !isProd && path.resolve(__dirname, 'src')
+
+module.exports = {
+  apps: [
+    {
+      name: 'jobs-server',
+      script: 'src/index.js',
+      instances: 1,
+      autorestart: true,
+      watch,
+      max_memory_restart: '1G'
+    },
+    {
+      name: 'jobs-bot',
+      script: 'src/bot.js',
+      instances: 1,
+      autorestart: true,
+      watch,
+      max_memory_restart: '1G'
+    }
+  ]
+}
