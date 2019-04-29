29386334c56d68877793a4e2b4d3240fb0f284d1
Mauricio Allende <mauricio.allende.o@gmail.com>
2019-04-28 22:04:08 -0400

    add getonbrd scrapper

---
+++ src/components/getonbrd/constants.js
@@ -0,0 +1,18 @@
+export const SOURCE_NAME = 'getonbrd'
+export const SALARY_STEP = 50
+export const SALARY_RANGE = [500, 7000]
+export const DEFAULT_HEADERS = {
+  'accept-language': 'es-US,es;q=0.9,es-419;q=0.8,en;q=0.7',
+  accept: '*/*'
+}
+
+export const CACHE_CONFIG_KEY = `${SOURCE_NAME}.config`
+export const CACHE_CSFR_KEY = `${SOURCE_NAME}.csfr`
+export const CACHE_SESSION_KEY = `${SOURCE_NAME}.session`
+export const CACHE_SALARIES_MAP_KEY = `${SOURCE_NAME}.salariesMap`
+export const CACHE_COMPANIES_KEY = `${SOURCE_NAME}.companies`
+export const CACHE_JOBS_MAP_KEY = `${SOURCE_NAME}.jobs`
+
+export const QUEUE_GET_JOBS = `${SOURCE_NAME}.getJobs`
+export const QUEUE_GET_SALARIES = `${SOURCE_NAME}.getSalaries`
+export const QUEUE_GET_COMPANIES = `${SOURCE_NAME}.getCompanies`
