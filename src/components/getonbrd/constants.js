00a4c6178681bba44f3389f2436442770f940e61
Mauricio Allende <mauricio.allende.o@gmail.com>
2019-05-01 22:38:08 -0400

    add queue and salary range cache constants

--- src/components/getonbrd/constants.js
+++ src/components/getonbrd/constants.js
@@ -8,6 +8,8 @@ export const CACHE_SESSION_KEY = `${SOURCE_NAME}.session`
 export const CACHE_SALARIES_MAP_KEY = `${SOURCE_NAME}.salariesMap`
 export const CACHE_COMPANIES_KEY = `${SOURCE_NAME}.companies`
 export const CACHE_JOBS_MAP_KEY = `${SOURCE_NAME}.jobs`
+export const CACHE_JOBS_QUEUED_KEY = `${SOURCE_NAME}.jobs.queued`
+export const CACHE_SALARY_RANGE_KEY = `${SOURCE_NAME}.salaryRanges`
 
 export const QUEUE_GET_JOBS = `${SOURCE_NAME}.getJobs`
 export const QUEUE_GET_SALARIES = `${SOURCE_NAME}.getSalaries`
