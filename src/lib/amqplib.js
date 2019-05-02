6f2c28ef317e82477b7c9ea7618aeb5e4fd4f6ff
Mauricio Allende <mauricio.allende.o@gmail.com>
2019-05-01 22:38:49 -0400

    add error callback parameter to amqplib

--- src/lib/amqplib.js
+++ src/lib/amqplib.js
@@ -8,7 +8,7 @@ const url = user && pass ? `amqp://${user}:${pass}@${host}` : `amqp://${host}`
 
 export const open = amqplib.connect(url)
 
-export const createQueue = async (queueName, opts, onMsg) => {
+export const createQueue = async (queueName, opts, onMsg, onError) => {
   const { prefetch = 1, assert = [] } = opts
 
   try {
@@ -18,9 +18,18 @@ export const createQueue = async (queueName, opts, onMsg) => {
 
     await Promise.all([...assert, queueName].map(q => ch.assertQueue(q)))
 
-    ch.consume(queueName, msg => onMsg(msg, ch), { noAck: false })
+    ch.consume(
+      queueName,
+      msg =>
+        onMsg(msg, ch).catch(
+          err => onError && onError(ch, msg, err, queueName)
+        ),
+      { noAck: false }
+    )
 
     logger.info(`[${queueName}] Waiting for messages.`)
+
+    return ch
   } catch (err) {
     logError(err)
   }
