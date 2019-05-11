31d189d4380e38906ed5c8621604537d59424acd
Mauricio Allende <mauricio.allende.o@gmail.com>
2019-05-11 16:52:43 -0400

    replace pino with winston (sonic-boom bug)

--- src/lib/logger.ts
+++ src/lib/logger.ts
@@ -1,13 +1,26 @@
-import pino from 'pino'
+import winston from 'winston'
 import config from '../config'
 import Sentry from './sentry'
 
-export const logger = pino()
+export const logger = winston.createLogger({
+  level: 'info',
+  format: winston.format.json(),
+  transports: [
+    new winston.transports.File({ filename: 'error.log', level: 'error' }),
+    new winston.transports.File({ filename: 'combined.log' })
+  ]
+})
+
+if (!config.isProd) {
+  logger.add(
+    new winston.transports.Console({
+      format: winston.format.simple()
+    })
+  )
+}
 
 export const logError = (err: Error, prefix?: string) => {
-  if (!err) {
-    return
-  }
+  if (!err) return
 
   if (config.isProd) {
     Sentry.captureException(err)
