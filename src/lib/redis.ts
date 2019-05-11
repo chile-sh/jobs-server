21381dfe8db2884f48aaa8d4a361f2e438f2c8d3
Mauricio Allende <mauricio.allende.o@gmail.com>
2019-05-11 16:55:21 -0400

    move setKeyExp to redis lib

--- src/lib/redis.ts
+++ src/lib/redis.ts
@@ -3,13 +3,19 @@ import Redis from 'ioredis'
 import config from '../config'
 import { logError } from './logger'
 
-interface JsonRedis extends Redis.Redis {
+interface CustomRedis extends Redis.Redis {
   hsetJson?(key: string, field: string, val: any): Promise<any>
-  hgetJson?(kwy: string, field: string): Promise<any>
+  hgetJson?(key: string, field: string): Promise<any>
+  setKeyExp?(
+    key: string,
+    seconds: number,
+    isHash: boolean,
+    expThreshold?: number
+  ): Promise<any>
 }
 
-const createClient = (opts = {}): JsonRedis => {
-  const client: JsonRedis = new Redis({
+const createClient = (opts = {}): CustomRedis => {
+  const client: CustomRedis = new Redis({
     host: config.redis.host,
     password: config.redis.password,
     db: 0,
@@ -22,14 +28,40 @@ const createClient = (opts = {}): JsonRedis => {
 
   client.hgetJson = async (key, field) => {
     const str = await client.hget(key, field)
-    if (!str) {
-      return str
-    }
+    if (!str) return str
 
     try {
       return JSON.parse(str)
     } catch (err) {
       return str
+    }
+  }
+
+  /**
+   * Set expiration in seconds for a given redis key
+   * If the remaining TTL is lower than the value set on `expThreshold`,
+   * it will empty the key/hash.
+   *
+   * @param key
+   * @param seconds
+   * @param isHash
+   * @param expThreshold EXP in seconds
+   *
+   * @example
+   *  redis.setKeyExp('cache', 3600 * 24, true, 3600)
+   */
+  client.setKeyExp = async (key, seconds, isHash, expThreshold = 0) => {
+    const create = () =>
+      isHash ? client.hset(key, '', '') : client.set(key, '')
+
+    const exists = await client.exists(key)
+    if (!exists) await create()
+
+    const ttl = await client.ttl(key)
+    if (ttl < expThreshold) {
+      await client.del(key)
+      await create()
+      return client.expire(key, seconds)
     }
   }
 
